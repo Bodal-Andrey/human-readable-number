6f1e7d5193887f4d6c5c6367ca6ab93458a0c1b0
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-07-26 15:56:38 +0500

    fix: delete const.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-import { FIRST_TEN, SECOND_TEN, TENS } from "./const";
+const FIRST_TEN = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten'
+};
+
+const SECOND_TEN = {
+    1: 'eleven',
+    2: 'twelve',
+    3: 'thirteen',
+    4: 'fourteen',
+    5: 'fifteen',
+    6: 'sixteen',
+    7: 'seventeen',
+    8: 'eighteen',
+    9: 'nineteen',
+    0: 'twenty'
+};
+
+const TENS = {
+    1: 'ten',
+    2: 'twenty',
+    3: 'thirty',
+    4: 'forty',
+    5: 'fifty',
+    6: 'sixty',
+    7: 'seventy',
+    8: 'eighty',
+    9: 'ninety'
+};
 
 module.exports = function toReadable(number) {
     const strNum = number.toString();
